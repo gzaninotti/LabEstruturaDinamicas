480d6c7296bddde47eb4de6f6bed264ddba2f059
Gabriel Zaninotti <gabriel@zaninotti.net>
2023-04-03 19:31:03 -0300

    First Commit

---
+++ CadastroAlunos/Pessoa.java
@@ -0,0 +1,32 @@
+
+/**
+ * Representa um,a pessoa.
+ * 
+ * @author Julio Arakaki
+ * @version 27/03/2023
+ */
+public class Pessoa {
+    // variaveis de instancia 
+    private int idade;
+    private NomePessoa nome;
+    
+    /**
+     * Pessoa chamado para instanciar uma pessoa
+     *
+     * @param nome String, nome da pessoa
+     * @param idade int, idade da pessoa
+     */
+    public Pessoa() {
+
+    }
+
+    
+    /**
+     * toString - imprime os dados da pessoa
+     *
+     * @return String, dados da pessoa
+     */
+    public String toString(){
+        return "";
+    }   
+}
