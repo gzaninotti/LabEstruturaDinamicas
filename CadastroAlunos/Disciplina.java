3133115c2b6ff1eab17716429188633507a5d1ff
Gabriel Zaninotti <gabriel@zaninotti.net>
2023-04-04 00:59:12 -0300

    Add/Fix Disciplina class
    Extending Disciplina to Texto class
    Added Getters/Setters
    Added toString

--- CadastroAlunos/Disciplina.java
+++ CadastroAlunos/Disciplina.java
@@ -1,18 +1,38 @@
-
 /**
  * Escreva uma descrição da classe Disciplina aqui.
  * 
- * @author (seu nome) 
- * @version (um número da versão ou uma data)
+ * @author Gabriel Zaninotti
+ * @version 03/04/2023
  */
-public class Disciplina {
+public class Disciplina extends Texto {
     private float nota;
-    Texto nomeDisc;
-
+    
     /**
      * Construtor para objetos da classe Disciplina
      */
-    public Disciplina() {
+    public Disciplina(String nome, float nota) {
+        super(nome);
+        setNota(nota);
     }
-
+    
+    private void setNota(float nota) {
+        this.nota = nota;
+    }
+    
+    protected String getNome() {
+        return getTxt();
+    }
+    
+    protected float getNota() {
+        return nota;
+    }
+    
+    /**
+     * toString - imprime os dados da disciplina
+     *
+     * @return String, dados da pessoa
+     */
+    public String toString(){
+        return getNome() + ", " + getNota();
+    } 
  }
