d4bb6e4ad66319602ea04af9f2226dd22654c27a
Gabriel Zaninotti <gabriel@zaninotti.net>
2023-04-04 01:01:07 -0300

    Add/Fix Pessoa Class
    Added extend to NomePessoa Class
    Added Getters/Setters
    Fixed Constructor
    Fixed toString

--- CadastroAlunos/Pessoa.java
+++ CadastroAlunos/Pessoa.java
@@ -1,11 +1,11 @@
-
+import java.util.ArrayList;
 /**
- * Representa um,a pessoa.
+ * Representa uma pessoa.
  * 
- * @author Julio Arakaki
- * @version 27/03/2023
+ * @author Gabriel Zaninotti
+ * @version 03/04/2023
  */
-public class Pessoa {
+public class Pessoa extends NomePessoa{
     // variaveis de instancia 
     private int idade;
     private NomePessoa nome;
@@ -16,10 +16,22 @@ public class Pessoa {
      * @param nome String, nome da pessoa
      * @param idade int, idade da pessoa
      */
-    public Pessoa() {
-
+    public Pessoa(String nome, int idade) {
+        super(nome);
+        setIdade(idade);
     }
 
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    public int getIdade() {
+        return this.idade;
+    }  
+    
+    public String getNomePessoa() {
+        return getNome();
+    }
     
     /**
      * toString - imprime os dados da pessoa
@@ -27,6 +39,6 @@ public class Pessoa {
      * @return String, dados da pessoa
      */
     public String toString(){
-        return "";
+        return getNome() + ", " + getIdade();
     }   
 }
